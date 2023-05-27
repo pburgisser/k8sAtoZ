863de0aec51a7b1ca7d4090dcd3ada2f5c5c006a
Philippe Bürgisser <philippe.burgisser@camptocamp.com>
2023-05-26 21:19:24 -0400

    feat: initial commit

---
+++ webapp-1/src/app.py
@@ -0,0 +1,15 @@
+from datetime import date
+from flask import Flask, request, jsonify
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return jsonify(
+        hostname=socket.gethostname(),
+        requestIP=request.remote_addr
+    )
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
---
+++ webapp-1/src/entrypoint.sh
@@ -0,0 +1,1 @@
+gunicorn -b 0.0.0.0:8080 --workers=1 wsgi:app --timeout 200 --log-file -
